11bd725d812016986fe36079de151613717eadbe
Tyler Maran <tyler.maran@gmail.com>
2018-05-07 23:08:26 -0400

    heroku push

--- app/data/food.js
+++ app/data/food.js
@@ -1,74 +1,74 @@
 // food is an array of possible restaurants
 
 var food = [{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    name: "Heirloom Market BBQ",
+    photo: "https://cdn.vox-cdn.com/thumbor/OBD-phCIuOks9dqLqeQPUqHtVFE=/0x0:1600x900/520x390/filters:focal(672x322:928x578):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/58263397/Heirloom_20Market_20BBQ_20-_20EATL_20-_2001-2.0.0.0.0.jpg",
     scores: [
+      "1",
       "2",
-      "1",
-      "1",
+      "3",
       "3",
       "2",
-      "1"
+      "2"
     ]
   }, {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    name: "Porch Light Latin Kitchen",
+    photo: "https://cdn.vox-cdn.com/thumbor/ABhmbzwMlqTTnx2w_oOi_LHrU20=/0x0:960x639/520x390/filters:focal(404x244:556x396):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/58263399/12509582_1135502633161887_6547723717684186674_n.0.0.jpg",
     scores: [
       "2",
+      "3",
+      "3",
       "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Atlas",
+    photo: "https://cdn.vox-cdn.com/thumbor/N8IRpB8tq-VUEtWkA98kqx7QgoQ=/0x0:5616x3744/520x390/filters:focal(2359x1423:3257x2321):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/58263403/Atlas_-_005_-_Brian_Gassel.0.0.jpg",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Desta Ethiopian Kitchen",
+    photo: "https://cdn.vox-cdn.com/thumbor/EnyaSpBoS1UhOguR4z8UL98QSmQ=/0x0:960x641/520x390/filters:focal(404x245:556x397):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/58263407/12047013_1038039689553141_793432965410667820_n.0.0.jpg",
+    scores: [
+      "3",
       "1",
       "3",
+      "3",
+      "2",
+      "2"
+    ]
+  },
+  {
+    name: "Royal Myanmar Cuisine",
+    photo: "https://cdn.vox-cdn.com/thumbor/lux8m4krydEzytOlih7vBFq68ls=/0x0:1000x1000/520x390/filters:focal(420x420:580x580):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/58263409/RoyalMyanmarGraceL.0.jpg",
+    scores: [
+      "3",
+      "3",
+      "2",
+      "2",
       "2",
       "1"
     ]
   },
   {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    name: "Cakes & Ale",
+    photo: "https://cdn.vox-cdn.com/thumbor/szAxsqKv7bQonHuKmCNmIftS1Zk=/0x0:960x637/520x390/filters:focal(404x243:556x395):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/58263431/cakes.0.0.jpg",
     scores: [
       "2",
-      "1",
-      "1",
-      "3",
       "2",
-      "1"
-    ]
-  },
-  {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: [
       "2",
-      "1",
-      "1",
-      "3",
       "2",
-      "1"
-    ]
-  },
-  {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: [
       "2",
-      "1",
-      "1",
-      "3",
-      "2",
-      "1"
-    ]
-  },
-  {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: [
-      "2",
-      "1",
-      "1",
-      "3",
-      "2",
-      "1"
+      "2"
     ]
   }
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,70 +16,52 @@ module.exports = function (app) {
         res.json(questions);
     });
 
-    
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // ---------------------------------------------------------------------------
-
     app.post("/api/food", function (req, res) {
-        // Note the code here. Our "server" will respond to a user"s survey result
-        // Then compare those results against every user in the database.
-        // It will then calculate the difference between each of the numbers and the user"s numbers.
-        // It will then choose the user with the least differences as the "best friend match."
-        // In the case of multiple users with the same result it will choose the first match.
-        // After the test, it will push the user to the database.
 
-        // We will use this object to hold the "best match". We will constantly update it as we
-        // loop through all of the options
         var bestMatch = {
-            name: "",
+            food: "",
             photo: "",
-            friendDifference: Infinity
         };
 
-        // Here we take the result of the user"s survey POST and parse it.
         var userData = req.body;
         console.log(userData);
+        console.log(food);
 
         // UserScores is an array of question answers (Agree = 1; Meh = 2)
-        var userScores = userData.scores;
-
-        // This variable will calculate the difference between the user"s scores and the scores of
-        // each user in the database
-        var totalDifference;
-
-        // Here we loop through all the friend possibilities in the database.
-        for (var i = 0; i < food.length; i++) {
-            var currentFriend = food[i];
-            totalDifference = 0;
-
-            console.log(currentFriend.name);
-
-            // We then loop through all the scores of each friend
-            for (var j = 0; j < currentFriend.scores.length; j++) {
-                var currentFriendScore = currentFriend.scores[j];
-                var currentUserScore = userScores[j];
-
-                // We calculate the difference between the scores and sum them into the totalDifference
-                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+        var userAnswers = userData['questions[]'];
+        console.log(userAnswers);
+        var score = 0;
+        var topScore = [];
+        for (let i = 0; i < food.length; i++) {
+            for (let j = 0; j < food[i].scores.length; j++) {
+                for (let k = 0; k < userAnswers.length; k++) {
+                    if (userAnswers[k] === food[i].scores[k]) {
+                        score++;
+                    }
+                }
             }
-
-            // If the sum of differences is less then the differences of the current "best match"
-            if (totalDifference <= bestMatch.friendDifference) {
-                // Reset the bestMatch to be the new friend.
-                bestMatch.name = currentFriend.name;
-                bestMatch.photo = currentFriend.photo;
-                bestMatch.friendDifference = totalDifference;
-            }
+        topScore.push(score);
+        score = 0;   
         }
+        console.log(topScore);
+        console.log(Math.max.apply(null, topScore));
+        for (let i = 0; i < topScore.length; i++) {
+            
+            if (Math.max.apply(null, topScore)=== topScore[i]) {
+                console.log("Matches " + i + " position");
+                bestMatch.food = food[i].name;
+                bestMatch.photo = food[i].photo;
+                console.log(bestMatch);
+                return res.json(bestMatch);
+            }
+            
+        }
+       
+        // // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+        // // the database will always return that the user is the user's best friend).
+        // food.push(userData);
 
-        // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-        // the database will always return that the user is the user's best friend).
-        food.push(userData);
-
-        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
-        res.json(bestMatch);
+        // // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+        
     });
 };
